07fda8b6243443a3813690d5d8e608d53fabd99e
Wendy Liga <wendy.devs@gmail.com>
2020-01-12 19:42:26 +0700

    update lisence

--- Sources/SwiftKit/String+Extension.swift
+++ Sources/SwiftKit/String+Extension.swift
@@ -1,3 +1,27 @@
+/**
+ SwiftKit
+ 
+ Copyright (c) 2020 Wendy Liga. Licensed under the MIT license, as follows:
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
 import Foundation
 
 extension String {
@@ -19,7 +43,7 @@ extension String {
      print(nameWithIdentation) -> "    Wendy Liga"
      ```
      - Parameters:
-        - count: number of identation
+     - count: number of identation
      
      - Returns: String with prefix Identation
      */
@@ -48,8 +72,8 @@ extension String {
      ```
      
      - Parameters:
-          - with: right side text
-          - offset: how many offset from left side
+     - with: right side text
+     - offset: how many offset from left side
      
      - Returns: String with separation, look Example for mode detail
      */
@@ -80,8 +104,8 @@ extension String {
      ```
      
      - Parameters:
-         - prefix: text you want to remove
-         - replaceWith: if you want to replace with spesific string
+     - prefix: text you want to remove
+     - replaceWith: if you want to replace with spesific string
      
      - Returns: String with remove prefix
      */
@@ -95,25 +119,25 @@ extension String {
     }
     
     /**
-    remove suffix on string
-    
-    Example:
-    ```
-    let string = "10.000."
-    let stringWithoutPrefix = string.withoutPrefix(".") -> "10.000"
-    
-    or
-    
-    let string = "~/document"
-    let stringWithoutPrefix = string.withoutPrefix(".") -> "~/document"
-    ```
-    
-    - Parameters:
-        - suffix: text you want to remove
-        - replaceWith: if you want to replace with spesific string
-    
-    - Returns: String with remove suffix
-    */
+     remove suffix on string
+     
+     Example:
+     ```
+     let string = "10.000."
+     let stringWithoutPrefix = string.withoutPrefix(".") -> "10.000"
+     
+     or
+     
+     let string = "~/document"
+     let stringWithoutPrefix = string.withoutPrefix(".") -> "~/document"
+     ```
+     
+     - Parameters:
+     - suffix: text you want to remove
+     - replaceWith: if you want to replace with spesific string
+     
+     - Returns: String with remove suffix
+     */
     public func withoutSuffix(_ suffix: String, replaceWith newString: String = "") -> String {
         guard hasSuffix(suffix) else {
             return self
@@ -137,9 +161,9 @@ extension Array where Element == String {
      
      Example:
      ```
-        let array = ["Apple", "Banana", "Orange"]
+     let array = ["Apple", "Banana", "Orange"]
      
-        array.getFormattedValue() -> Apple, Banana, Orange
+     array.getFormattedValue() -> Apple, Banana, Orange
      ```
      
      Returns: String value with comma separated
